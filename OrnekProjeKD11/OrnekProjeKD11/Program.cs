43121a245bd615c42245b14487b6c4dfe0222f34
rmutluer <resul.mutluer@hotmail.com>
2022-06-10 12:08:50 +0300

    Toplama metodu eklendi

--- OrnekProjeKD11/OrnekProjeKD11/Program.cs
+++ OrnekProjeKD11/OrnekProjeKD11/Program.cs
@@ -7,10 +7,19 @@ namespace OrnekProjeKD11
         static void Main(string[] args)
         {
             Console.WriteLine("Merhaba GitHub!");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            int sayi2= Convert.ToInt32(Console.ReadLine());
+            int toplam = ToplamaYap(sayi1, sayi2);
+
 
             string isim = "Bilge Adam";
             EkranaYazdir(isim);
             Console.ReadLine();
+        }
+
+        private static int ToplamaYap(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
         }
 
         private static void EkranaYazdir(string isim)
